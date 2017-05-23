8f7e87b90b1a547cb800ccaea8e4f0e4d38f8a87
Greg Munker <gmunker@gmail.com>
2017-05-22 19:12:49 -0700

    syntax error

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect(`http://${req.hostname}${req.url}`)
-  } else {
-    next();
-  }
-});
+// app.use((req, res, next) => {
+//   if (req.headers['x-forwarded-proto'] === 'https') {
+//     res.redirect(`http://${req.hostname}${req.url}`)
+//   } else {
+//     next();
+//   }
+// });
 
   app.use(express.static(__dirname, '/dist'));
   app.get('*', (req, res) => {
