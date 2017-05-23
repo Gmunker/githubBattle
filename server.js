3ae537a94e05f874d8a148cdbadcbb27a27c6ba9
Greg Munker <gmunker@gmail.com>
2017-05-22 21:51:44 -0700

    ..

--- server.js
+++ server.js
@@ -4,13 +4,6 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect(`http://${req.hostname}${req.url}`)
-  } else {
-    next();
-  }
-});
 
   app.use(express.static((__dirname + '/dist')));
 
