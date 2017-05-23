e83c96b2d1a1d9a09986917203ba817e3667fe42
Greg Munker <gmunker@gmail.com>
2017-05-22 22:17:11 -0700

    ..

--- server.js
+++ server.js
@@ -10,17 +10,11 @@ app.use((req, res, next) => {
   }
 });
 
-  app.use(express.static(__dirname, '/dist'));
+  app.use(express.static((__dirname + '/dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './dist', 'index.html'));
   });
   
-  app.listen( process.env.PORT || 3000, () => {
-    let port = "";
-    if (process.env.PORT) {
-      port = JSON.stringify(process.env.PORT);
-    } else {
-      port = 3000;
-    }
-    console.log(`Express server is u on port ${ process.env.PORT || 3000}`)
+  app.listen(process.env.PORT || 3000, function () {
+	console.log('Express server is up on port ' + process.env.PORT || 3000);
 });
