d954a2fe23b2ddddcb9cb47fc76f7b5854ae6ae9
Greg Munker <gmunker@gmail.com>
2017-05-22 19:15:50 -0700

    syntax error

--- server.js
+++ server.js
@@ -2,25 +2,19 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-// app.use((req, res, next) => {
-//   if (req.headers['x-forwarded-proto'] === 'https') {
-//     res.redirect(`http://${req.hostname}${req.url}`)
-//   } else {
-//     next();
-//   }
-// });
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'https') {
+    res.redirect(`http://${req.hostname}${req.url}`)
+  } else {
+    next();
+  }
+});
 
-  app.use(express.static(__dirname, '/dist'));
+  app.use(express.static((__dirname + '/dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/dist', 'index.html'));
+    res.sendFile(path.join(__dirname, '/dist', 'index.html'));
   });
   
-  app.listen( process.env.PORT || 3000, () => {
-    let port = "";
-    if (process.env.PORT) {
-      port = JSON.stringify(process.env.PORT);
-    } else {
-      port = 3000;
-    }
-    console.log(`Express server is u on port ${ process.env.PORT || 3000}`)
+  app.listen(process.env.PORT || 3000, function () {
+	console.log('Express server is up on port ' + PORT);
 });
