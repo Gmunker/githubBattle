82e3b43ff36a9b9b29ea1e0ce026f5490b755305
Greg Munker <gmunker@gmail.com>
2017-05-22 22:18:03 -0700

    ..

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+var express = require('express');
+var path = require('path');
 
-app.use((req, res, next) => {
-  if (req.headers['x-forwarded-proto'] === 'https') {
-    res.redirect(`http://${req.hostname}${req.url}`)
-  } else {
-    next();
-  }
-});
+// Create our app
+var app = express();
+const PORT = process.env.PORT || 3000;
 
-  app.use(express.static((__dirname + '/dist')));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './dist', 'index.html'));
-  });
-  
-  app.listen(process.env.PORT || 3000, function () {
-	console.log('Express server is up on port ' + process.env.PORT || 3000);
+app.use(express.static((__dirname + '/dist')));
+
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, './dist', 'index.html'))
+})
+
+app.listen(PORT, function () {
+	console.log('Express server is up on port ' + PORT);
 });
