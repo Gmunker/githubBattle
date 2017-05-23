c5876408586d5363ea4d5830d1588f3554d9cd99
Greg Munker <gmunker@gmail.com>
2017-05-22 22:23:28 -0700

    ..

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ app.use((req, res, next) => {
   }
 });
 
-  app.use(express.static(__dirname, '/dist'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '/dist', 'index.html'));
-  });
+ app.use(express.static((__dirname + '/dist')));
+
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, './dist', 'index.html'))
+})
   
   app.listen( process.env.PORT || 3000, () => {
     let port = "";
