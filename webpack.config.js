d18d900483dd5dec3dfc688a4f27cb870d8faaaf
Greg Munker <gmunker@gmail.com>
2017-05-22 22:19:11 -0700

    ..

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
 
-let config = {
-  entry: './app/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js',
-    publicPath: '/'
-  },
-  module: {
-    rules: [
-      { test: /\.(js)$/, use: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
-    ]
-  },
-  devServer: {
-    historyApiFallback: true
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'app/index.html'
-    })
-  ]
-}
+var config = {
+	entry: './app/index.js',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'index_bundle.js',
+		publicPath: '/'
+	},
+	module: {
+		rules: [
+			{ test: /\.(js)$/, use: 'babel-loader'},
+			{ test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+		]
+	},
+	devServer: {
+		historyApiFallback: true
+	},
+	plugins: [new HtmlWebpackPlugin({
+		template: 'app/index.html'
+	})]
+};
 
 if (process.env.NODE_ENV === 'production') {
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      'process.env': {
-        'SET NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  )
+	config.plugins.push(
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin()
+	);
 }
 
 module.exports = config;
