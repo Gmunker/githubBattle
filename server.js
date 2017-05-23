3a8cabf61a5a81ffa11b327b0dd94bcc8f9ef153
Greg Munker <gmunker@gmail.com>
2017-05-22 20:19:03 -0700

    syntax error

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
 
   app.use(express.static((__dirname + '/dist')));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/dist', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './dist', 'index.html'));
   });
   
   app.listen(process.env.PORT || 3000, function () {
