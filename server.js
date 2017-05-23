9ae36292c719beecd4966863a65769e0154405f2
Greg Munker <gmunker@gmail.com>
2017-05-22 21:55:52 -0700

    ..

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require('express');
-const path = require('path');
+var express = require('express');
+var path = require('path');
 
-const app = express();
+// Create our app
+var app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static((__dirname + '/dist')));
 
-  app.use(express.static((__dirname + '/dist')));
+app.get('*', function (request, response){
+  response.sendFile(path.resolve(__dirname, './dist', 'index.html'))
+})
 
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './dist', 'index.html'));
-  });
-  
-  app.listen(PORT, function () {
-	console.log(`Express server is up on port ${PORT}`);
+app.listen(PORT, function () {
+	console.log('Express server is up on port ' + PORT);
 });
